8ef94999bb114afff4f5e64d4d99cd824dccfd46
satyam-chauhan-21 <archauhan4390@gmail.com>
2024-01-04 22:25:02 +0530

    created first file

---
+++ FirstJava.java
@@ -0,0 +1,10 @@
+public class FirstJava {
+    public static void main(String[] args){
+        System.out.println("//--------------------------------------------------------------------//");
+        System.out.println("This is my first Java program file or class which I clone from Github.");
+        System.out.println("to run this file in Terminal first write javac file_name.java then press Enter. ");
+        System.out.println("when we done above process then a .class file is generated in similar folder in which our file is present.");
+        System.out.println("now we have to just simply execute this .class file using this command => java file_name (here we no need to write .class extension");
+        System.out.println("//--------------------------------------------------------------------//");
+    }
+}
